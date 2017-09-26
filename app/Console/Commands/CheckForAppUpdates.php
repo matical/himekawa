18c0ce33b7a27467676076d9fd40e8718dfbb76c
matical <maticalssh@gmail.com>
2017-09-26 16:06:48 +0800

    Basic implementation to auto fetch and update apks

--- app/Console/Commands/CheckForAppUpdates.php
+++ app/Console/Commands/CheckForAppUpdates.php
@@ -3,9 +3,10 @@
 namespace himekawa\Console\Commands;
 
 use himekawa\WatchedApp;
+use yuki\Scrapers\Download;
 use yuki\Scrapers\Metainfo;
+use yuki\Scrapers\Versioning;
 use Illuminate\Console\Command;
-use Illuminate\Support\Facades\Cache;
 
 class CheckForAppUpdates extends Command
 {
@@ -24,6 +25,18 @@ class CheckForAppUpdates extends Command
     protected $description = 'Check for available updates.';
 
     /**
+     * @var array An array containing all app metadata, indexed by the package name
+     */
+    protected $appMetadata = [];
+
+    /**
+     * Array of apps with available updates.
+     *
+     * @var array
+     */
+    protected $appsRequiringUpdates = [];
+
+    /**
      * Create a new command instance.
      *
      * @return void
@@ -36,26 +49,41 @@ class CheckForAppUpdates extends Command
     /**
      * Execute the console command.
      *
-     * @param \yuki\Scrapers\Metainfo $metainfo
+     * @param \yuki\Scrapers\Metainfo   $metainfo
+     * @param \yuki\Scrapers\Versioning $versioning
+     * @param \yuki\Scrapers\Download   $download
      * @return mixed
      */
-    public function handle(Metainfo $metainfo)
+    public function handle(Metainfo $metainfo, Versioning $versioning, Download $download)
     {
-        $metadata = $this->fetchAppMetadata($metainfo);
+        $this->appMetadata = $this->fetchAppMetadata($metainfo);
+
+        foreach ($this->appMetadata as $app) {
+            // Queue up the apps that have updates pending
+            if ($versioning->areUpdatesAvailable($app->packageName, $app->versionCode)) {
+                $this->appsRequiringUpdates[] = $app;
+            }
+        }
+
+        foreach ($this->appsRequiringUpdates as $app) {
+            $download->build($app->packageName, $app->versionCode, $app->sha1)
+                     ->run()
+                     ->store();
+        }
     }
 
     /**
      * Fetch metadata based on the watch list.
      *
-     * @param $metainfo
+     * @param \yuki\Scrapers\Metainfo $metainfo
      * @return array|null An array containing all app metadata, indexed by the package name
      */
-    protected function fetchAppMetadata($metainfo)
+    protected function fetchAppMetadata(Metainfo $metainfo)
     {
         $watchedPackages = WatchedApp::pluck('package_name');
 
         foreach ($watchedPackages as $package) {
-            $this->info('Checking ' . $package . '...');
+            $this->info("Checking $package...");
             $fetchMetadata = $metainfo->make();
 
             $result[$package] = metaCache($package, $fetchMetadata);
