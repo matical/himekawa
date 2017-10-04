81f5e823c9b037b11f252ae2ac952453e1749924
matical <maticalssh@gmail.com>
2017-10-05 04:27:07 +0800

    Basic routes + views for faq

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,27 +2,14 @@
 
 namespace himekawa\Http\Controllers;
 
-use Illuminate\Http\Request;
-
 class HomeController extends Controller
 {
-    /**
-     * Create a new controller instance.
-     *
-     * @return void
-     */
-    public function __construct()
+    public function faq()
     {
-        $this->middleware('auth');
+        return view('frontend.faq');
     }
 
-    /**
-     * Show the application dashboard.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
+    public function about()
     {
-        return view('home');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,10 @@
 Route::get('/', 'AvailableAppController@index')
      ->name('index');
 
+Route::get('/about', 'HomeController@about')
+     ->name('index.about');
+Route::get('/faq', 'HomeController@faq')
+     ->name('index.faq');
+
 //Auth::routes();
 //Route::get('/home', 'HomeController@index')->name('home');
