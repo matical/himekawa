c79d0b9da299f62c0f305a19dff1f027f2e4058e
matical <maticalssh@gmail.com>
2017-12-24 22:35:32 +0800

    Find by slug

--- yuki/Repositories/WatchedAppsRepository.php
+++ yuki/Repositories/WatchedAppsRepository.php
@@ -7,10 +7,23 @@ use Illuminate\Support\Facades\Cache;
 
 class WatchedAppsRepository
 {
+    /**
+     * @return \himekawa\WatchedApp
+     */
     public function allApps()
     {
         return Cache::remember('watched-apps:all', config('googleplay.metainfo_cache_ttl'), function () {
             return WatchedApp::all();
         });
+    }
+
+    /**
+     * @param $slug
+     * @return \himekawa\WatchedApp
+     */
+    public function findBySlug($slug)
+    {
+        return WatchedApp::where('slug', $slug)
+                         ->first();
     }
 }
