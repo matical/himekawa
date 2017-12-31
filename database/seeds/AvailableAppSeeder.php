8c0f224623f38ce0b3ba137335ca3d43b09262db
matical <maticalssh@gmail.com>
2017-12-31 22:24:38 +0800

    Leave these out for now

--- database/seeds/AvailableAppSeeder.php
+++ database/seeds/AvailableAppSeeder.php
@@ -12,26 +12,26 @@ class AvailableAppSeeder extends Seeder
      */
     public function run()
     {
-        $deresute = WatchedApp::find(1);
-        $deresute->availableApps()->createMany([
-            [
-                'version_code' => 102,
-                'version_name' => '3.1.3',
-                'size'         => 63324820,
-                'hash'         => 'f75e1b7035ac3869e2398f55df56ebc3cbe9475e',
-            ],
-            [
-                'version_code' => 103,
-                'version_name' => '3.1.4',
-                'size'         => 63417240,
-                'hash'         => 'a2a3f91d19c70868500775dd048e0433b9a956',
-            ],
-            [
-                'version_code' => 104,
-                'version_name' => '3.2.0',
-                'size'         => 64296399,
-                'hash'         => '443aa273ed77c1001dc671688a22ae31eae69a28',
-            ],
-        ]);
+//        $deresute = WatchedApp::find(1);
+//        $deresute->availableApps()->createMany([
+//            [
+//                'version_code' => 102,
+//                'version_name' => '3.1.3',
+//                'size'         => 63324820,
+//                'hash'         => 'f75e1b7035ac3869e2398f55df56ebc3cbe9475e',
+//            ],
+//            [
+//                'version_code' => 103,
+//                'version_name' => '3.1.4',
+//                'size'         => 63417240,
+//                'hash'         => 'a2a3f91d19c70868500775dd048e0433b9a956',
+//            ],
+//            [
+//                'version_code' => 104,
+//                'version_name' => '3.2.0',
+//                'size'         => 64296399,
+//                'hash'         => '443aa273ed77c1001dc671688a22ae31eae69a28',
+//            ],
+//        ]);
     }
 }
