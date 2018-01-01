ad015938ac3ef676c1f148f10ba68d9bda834b33
matical <maticalssh@gmail.com>
2018-01-01 23:53:00 +0800

    Dummy user for the sake of sending notifications

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,5 +1,6 @@
 <?php
 
+use himekawa\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -11,6 +12,12 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+        User::create([
+            'name'     => 'sck',
+            'email'    => 'yuki@yuki.yuki',
+            'password' => '$2y$10$Ia5E/Pj32peHAs0SbNKN9e0fGhFRm5LDmv6zh9FGSiVBelKfTGAqu' // changeme
+        ]);
+
         $this->call(WatchedAppSeeder::class);
         $this->call(AvailableAppSeeder::class);
     }
