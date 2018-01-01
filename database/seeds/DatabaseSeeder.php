02b4e70fb203d90fbfc84cbfebce3083757d5cee
Amatsuka Mao <maticalssh@gmail.com>
2018-01-01 15:55:25 +0000

    Apply fixes from StyleCI

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -15,7 +15,7 @@ class DatabaseSeeder extends Seeder
         User::create([
             'name'     => 'sck',
             'email'    => 'yuki@yuki.yuki',
-            'password' => '$2y$10$Ia5E/Pj32peHAs0SbNKN9e0fGhFRm5LDmv6zh9FGSiVBelKfTGAqu' // changeme
+            'password' => '$2y$10$Ia5E/Pj32peHAs0SbNKN9e0fGhFRm5LDmv6zh9FGSiVBelKfTGAqu', // changeme
         ]);
 
         $this->call(WatchedAppSeeder::class);
