2781783ca00368ed10c68e9fafa4df767c537848
matical <maticalssh@gmail.com>
2018-01-02 00:28:51 +0800

    Unneeded routes

--- routes/web.php
+++ routes/web.php
@@ -2,11 +2,7 @@
 
 Route::get('/', 'AvailableAppController@index')
      ->name('index');
-
 Route::get('/about', 'HomeController@about')
      ->name('index.about');
 Route::get('/faq', 'HomeController@faq')
      ->name('index.faq');
-
-//Auth::routes();
-//Route::get('/home', 'HomeController@index')->name('home');
