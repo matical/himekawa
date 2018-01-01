e5f87bc78cec5e7e70f358cea2b78524ff1d071b
matical <maticalssh@gmail.com>
2018-01-01 21:35:42 +0800

    Tweak scheduler slightly because of cron idiosyncrasies

--- config/himekawa.php
+++ config/himekawa.php
@@ -5,6 +5,6 @@ return [
     'scheduler' => [
         'timezone'   => env('HIMEKAWA_SCHEDULER_TIMEZONE', 'Asia/Tokyo'),
         'start_time' => env('HIMEKAWA_SCHEDULER_START', '8:00'),
-        'end_time'   => env('HIMEKAWA_SCHEDULER_END', '18:00'),
+        'end_time'   => env('HIMEKAWA_SCHEDULER_END', '19:35'),
     ],
 ];
