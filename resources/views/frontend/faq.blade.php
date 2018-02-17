0033e0c8a3d143cc918795b0c7b2afafa0414673
matical <maticalssh@gmail.com>
2018-02-17 10:51:46 +0800

    Include new schedule

--- resources/views/frontend/faq.blade.php
+++ resources/views/frontend/faq.blade.php
@@ -3,10 +3,10 @@
 
 @section('content')
     <h2>Frequency</h2>
-    <p>The scraper runs every 15 minutes. Only on weekdays from {{ config('himekawa.scheduler.start_time') }} to {{ config('himekawa.scheduler.end_time') }} JST.</p>
+    <p>The scraper runs every 15 minutes on weekdays, every hour on weekends, from {{ config('himekawa.scheduler.start_time') }} to {{ config('himekawa.scheduler.end_time') }} JST.</p>
 
     <h2>Hashes</h2>
-    <p>Every hash (SHA1) displayed on each file is retrieved from GPlay (included in the payload metadata). The scraper automatically verifies each downloaded file; if there is a hash mismatch, the file is automatically discarded. (that means if it doesn't verify it's borked -- let me know thanks) </p>
+    <p>Every hash (SHA1) displayed on each file is retrieved from GPlay (included in the payload metadata). The scraper will automatically verifies each downloaded file; if there is a hash mismatch, the file is automatically discarded. (that means if it doesn't verify something is borked -- let me know thanks) </p>
 
     <h2>APK Lifetime</h2>
     <p>The system will keep up to 5 APKs per game.</p>
