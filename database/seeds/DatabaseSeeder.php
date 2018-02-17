a40fd4fa64fc2580936f1d1f0a4f328eb272e401
matical <maticalssh@gmail.com>
2018-02-17 11:33:47 +0800

    Seed route if available

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -13,9 +13,10 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         User::create([
-            'name'     => 'sck',
-            'email'    => 'yuki@yuki.yuki',
-            'password' => '$2y$10$Ia5E/Pj32peHAs0SbNKN9e0fGhFRm5LDmv6zh9FGSiVBelKfTGAqu', // changeme
+            'name'           => 'sck',
+            'email'          => 'yuki@yuki.yuki',
+            'password'       => '$2y$10$Ia5E/Pj32peHAs0SbNKN9e0fGhFRm5LDmv6zh9FGSiVBelKfTGAqu', // changeme
+            'telegram_token' => env('TELEGRAM_BOT_ROUTE', null),
         ]);
 
         $this->call(WatchedAppSeeder::class);
