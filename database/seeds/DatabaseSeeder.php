7f01b27a802d36c95e25d1987d27780fc6b22cca
matical <maticalssh@gmail.com>
2018-03-03 14:05:08 +0800

    Comment out the call instead

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -20,6 +20,6 @@ class DatabaseSeeder extends Seeder
         ]);
 
         $this->call(WatchedAppSeeder::class);
-        $this->call(AvailableAppSeeder::class);
+//        $this->call(AvailableAppSeeder::class);
     }
 }
