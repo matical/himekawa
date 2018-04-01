d428c4702b671ce04ece689b0e6f68cfe52386a4
matical <maticalssh@gmail.com>
2018-04-01 21:24:09 +0800

    Formatting

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -31,10 +31,10 @@ class Kernel extends HttpKernel
 //            \himekawa\Http\Middleware\EncryptCookies::class,
 //            \Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse::class,
 //            \Illuminate\Session\Middleware\StartSession::class,
-//             \Illuminate\Session\Middleware\AuthenticateSession::class,
+//            \Illuminate\Session\Middleware\AuthenticateSession::class,
 //            \Illuminate\View\Middleware\ShareErrorsFromSession::class,
 //            \himekawa\Http\Middleware\VerifyCsrfToken::class,
-\Illuminate\Routing\Middleware\SubstituteBindings::class,
+            \Illuminate\Routing\Middleware\SubstituteBindings::class,
         ],
 
         'api' => [
