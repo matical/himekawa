1e62c052c3f0cbea0dc678e4905ed8d333e20556
matical <maticalssh@gmail.com>
2018-04-02 11:41:20 +0800

    These libraries aren't actually used, excluding them

--- resources/assets/js/bootstrap.js
+++ resources/assets/js/bootstrap.js
@@ -1,17 +1,5 @@
 try {
-    window._ = require('lodash');
-    window.$ = window.jQuery = require('jquery');
-
+    window.Vue = require('vue');
+    window.VueMaterial = require('vue-material');
 } catch (e) {}
 
-window.axios = require('axios');
-window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
-
-let token = document.head.querySelector('meta[name="csrf-token"]');
-
-if (token) {
-    window.axios.defaults.headers.common['X-CSRF-TOKEN'] = token.content;
-}
-
-window.Vue = require('vue');
-window.VueMaterial = require('vue-material');
--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -6,8 +6,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="theme-color" content="#ff9800">
 
-    {{-- CSRF Token --}}
-    <meta name="csrf-token" content="{{ csrf_token() }}">
     <meta name="description" content="@yield('meta-description', 'ほ？')">
 
     <title>ひめかわ :: @yield('title')</title>
