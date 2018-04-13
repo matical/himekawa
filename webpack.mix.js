2138a41a05a1c2a7dfb9acb682b8f3eeaa2afd30
matical <maticalssh@gmail.com>
2018-04-13 13:56:25 +0800

    More attempts

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,12 +1,5 @@
 let mix = require('laravel-mix');
-let glob = require('glob');
 
-let dirs = [
-    {
-        source: 'resources/assets/images',
-        dest: 'public/images',
-    },
-];
 /*
  |--------------------------------------------------------------------------
  | Mix Asset Management
@@ -18,26 +11,14 @@ let dirs = [
  |
  */
 
-mix.copyDirectory('resources/assets/images', 'public/images')
-   .js('resources/assets/js/app.js', 'public/js')
+mix.js('resources/assets/js/app.js', 'public/js')
    .sass('resources/assets/sass/app.scss', 'public/css');
 
 
 mix.disableNotifications();
 
 if (mix.inProduction()) {
-    dirs.forEach((dir) => {
-        mix.copy(dir.source, dir.dest, false);
-    });
-
-    let files = [];
-    dirs.forEach((dir) => {
-        glob.sync('**/*', {cwd: dir.source}).forEach((file) => {
-            files.push(dir.dest + '/' + file);
-        });
-    });
-
-    mix.version(files);
+    mix.version('public/images');
 }
 
 if (! mix.inProduction()) {
