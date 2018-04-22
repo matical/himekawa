7a583524c396d6f4de772e1a55ab77d50da5d799
Amatsuka Mao <whitecaution@gmail.com>
2018-04-22 23:45:25 +0800

    Manual route

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,10 @@
 <?php
 
-Route::get('apps/list', 'Api\AvailableAppController@list');
-
-Route::resource('apps', 'Api\AvailableAppController')->only([
-    'index',
-    'show',
-]);
+Route::prefix('apps')->group(function () {
+    Route::get('/', 'AvailableAppController@index')
+         ->name('api.apps.index');
+    Route::get('list', 'AvailableAppController@list')
+         ->name('api.apps.list');
+    Route::get('{slug}', 'AvailableAppController@show')
+         ->name('api.apps.show');
+});
