73b1e91b2fa8b588ba75bdd565aed202ec5617a9
matical <maticalssh@gmail.com>
2018-04-25 17:21:32 +0800

    Testing http2 push

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -35,6 +35,7 @@ class Kernel extends HttpKernel
 //            \Illuminate\View\Middleware\ShareErrorsFromSession::class,
 //            \himekawa\Http\Middleware\VerifyCsrfToken::class,
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
+            \JacobBennett\Http2ServerPush\Middleware\AddHttp2ServerPush::class,
         ],
 
         'api' => [
