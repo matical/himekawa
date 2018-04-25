b0cc03fc0c73908674b73e6f2873440d3c858a50
Amatsuka Mao <maticalssh@gmail.com>
2018-04-25 14:49:10 +0800

    Info about RSS feeds

--- resources/views/frontend/faq.blade.php
+++ resources/views/frontend/faq.blade.php
@@ -15,8 +15,8 @@
         <h2 class="heavy">APK Lifetime</h2>
         <p>The system will keep up to 5 APKs per game.</p>
 
-        <h2 class="heavy">Telegram Bot Updates</h2>
-        <p>Join <a href="https://t.me/joinchat/AAAAAEwtuc6mq_TfZAzviA">this</a> telegram channel if you want to receive updates everytime an app here is updated.</p>
+        <h2 class="heavy">Notifications</h2>
+        <p><a href="https://t.me/joinchat/AAAAAEwtuc6mq_TfZAzviA">Telegram Channel</a> | <a href="{{ route('feeds.main') }}">RSS Feed</a></p>
 
         <h2 class="heavy">Adding APKs</h2>
         <p>As long as it's a weeb (JP region-locked) game, just drop me a message and I'll add it.</p>
