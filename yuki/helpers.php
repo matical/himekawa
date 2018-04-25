f03c90c65d338f4377a7f6975285a8485ede38e9
Amatsuka Mao <maticalssh@gmail.com>
2018-04-25 14:41:57 +0800

    Friendly bytes helper

--- yuki/helpers.php
+++ yuki/helpers.php
@@ -95,3 +95,23 @@ if (! function_exists('lastRun')) {
         return app('lastRun');
     }
 }
+
+if (! function_exists('humanReadableSize')) {
+    /**
+     * @param int $sizeInBytes
+     * @return string
+     */
+    function humanReadableSize(int $sizeInBytes): string
+    {
+        $units = ['B', 'KB', 'MB', 'GB', 'TB'];
+        if ($sizeInBytes === 0) {
+            return '0 ' . $units[1];
+        }
+
+        for ($i = 0; $sizeInBytes > 1024; $i++) {
+            $sizeInBytes /= 1024;
+        }
+
+        return round($sizeInBytes, 2) . ' ' . $units[$i];
+    }
+}
