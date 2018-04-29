42d291d001ed94522ac26ba197decdb1858f4a77
Amatsuka Mao <maticalssh@gmail.com>
2018-04-29 13:42:17 +0800

    Refactoring

--- yuki/Version.php
+++ yuki/Version.php
@@ -2,34 +2,80 @@
 
 namespace yuki;
 
+use Closure;
 use Illuminate\Support\Facades\Cache;
 
 class Version
 {
-    public function revision()
+    /**
+     * @var int
+     */
+    protected $minutes = 60;
+
+    /**
+     * @var string
+     */
+    protected $currentRevisionKey = 'version:current-revision';
+    /**
+     * @var string
+     */
+    protected $currentHashKey = 'version:current-hash';
+    /**
+     * @var string
+     */
+    protected $currentStateKey = 'version:current-state';
+
+    /**
+     * @return string|null
+     */
+    public function revision(): ?string
     {
-        return Cache::remember('version:current-revision', 60, function () {
-            return trim(shell_exec('git rev-list --count HEAD'));
+        return $this->cached($this->currentRevisionKey, function () {
+            return $this->exec('git rev-list --count HEAD');
         });
     }
 
-    public function hash()
+    /**
+     * @return string|null
+     */
+    public function hash(): ?string
     {
-        return Cache::remember('version:current-hash', 60, function () {
-            return trim(shell_exec('git rev-parse --short HEAD'));
+        return $this->cached($this->currentHashKey, function () {
+            return $this->exec('git rev-parse --short HEAD');
         });
     }
 
-    public function isClean()
+    /**
+     * @return bool
+     */
+    public function isClean(): bool
     {
-        return Cache::remember('version:current-state', 60, function () {
-            $porcelain = shell_exec('git status --porcelain');
+        return $this->cached($this->currentStateKey, function () {
+            $porcelain = $this->exec('git status --porcelain');
 
-            if ($porcelain) {
-                return false;
-            }
-
-            return true;
+            return (bool) $porcelain;
         });
+    }
+
+    /**
+     * @param $command
+     * @return string
+     */
+    protected function exec($command): ?string
+    {
+        return trim(shell_exec($command));
+    }
+
+    /**
+     * @param string   $key
+     * @param \Closure $callback
+     * @return mixed
+     */
+    protected function cached(string $key, Closure $callback)
+    {
+        return Cache::tags('metadata')
+                    ->remember($key, config('googleplay.metainfo_cache_ttl'), function () use ($callback) {
+                        return $callback();
+                    });
     }
 }
