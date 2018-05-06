6733d62c4a3c2c87323a55ac790ca4681d7ea917
Amatsuka Mao <maticalssh@gmail.com>
2018-05-06 19:34:26 +0800

    Check if items are countable

--- yuki/Command/HasPrettyProgressBars.php
+++ yuki/Command/HasPrettyProgressBars.php
@@ -2,6 +2,7 @@
 
 namespace yuki\Command;
 
+use InvalidArgumentException;
 use Symfony\Component\Console\Helper\ProgressBar;
 
 trait HasPrettyProgressBars
@@ -9,20 +10,35 @@ trait HasPrettyProgressBars
     /**
      * Create a new prettified progress bar.
      *
-     * @param int|mixed $count
+     * @param int|mixed $items Either an int or something countable can be passed
      * @return ProgressBar
      */
-    protected function newProgressBar($count): ProgressBar
+    protected function newProgressBar($items): ProgressBar
     {
-        if (! is_int($count)) {
-            $count = count($count);
-        }
+        $items = $this->normalizeCount($items);
 
-        return tap($this->output->createProgressBar($count), function (ProgressBar $bar) {
+        return tap($this->output->createProgressBar($items), function (ProgressBar $bar) {
             $bar->setBarCharacter('<fg=green>=</>');
             $bar->setEmptyBarCharacter('<fg=red>=</>');
             $bar->setProgressCharacter('<fg=green>></>');
-            $bar->setFormat("<fg=white;bg=cyan> %message:-45s%</>\n%current%/%max% [%bar%] %percent:3s%%\n🏁  %estimated:-20s%  %memory:20s%");
+            $bar->setFormat("<fg=white;bg=cyan> %message:-45s%</>\n%current%/%max% [%bar%] %percent:3s%%\n %estimated:-20s%  %memory:20s%");
         });
+    }
+
+    /**
+     * @param $items
+     * @return int
+     */
+    protected function normalizeCount($items)
+    {
+        if (is_int($items)) {
+            return $items;
+        }
+
+        if (is_countable($items)) {
+            return count($items);
+        }
+
+        throw new InvalidArgumentException('Expected int or countable');
     }
 }
