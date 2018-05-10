45d00c4f753b73aed0f2fd5167adb60491ea41c9
Amatsuka Mao <maticalssh@gmail.com>
2018-05-11 02:53:22 +0800

    Some flexin fixes

--- resources/assets/js/components/Himekawa.vue
+++ resources/assets/js/components/Himekawa.vue
@@ -11,12 +11,12 @@
             </div>
 
             <md-list slot="md-expand">
-                <md-list-item class="md-inset" v-for="(apk, index) in app.available_apps" :key="apk.app_id" @click="">
-                    <div class="md-layout md-alignment-center-left truncated">
+                <md-list-item class="md-inset md-layout" v-for="(apk, index) in app.available_apps" :key="apk.version_code" @click="">
+                    <div class="md-layout md-layout-item md-xsmall-size-85 md-size-70 md-alignment-center-left truncated">
                         <span :class="{ tags: true, latest: index === 0 }">v{{ apk.version_name }}</span>
                         <span class="md-layout md-alignment-center-left">{{ resolveApkFilename(app.package_name, apk.version_code) }}</span>
                     </div>
-                    <div class="md-layout md-alignment-center-right">
+                    <div class="md-layout md-layout-item md-xsmall-size-15 md-size-30 md-alignment-center-right">
                         <md-button class="md-raised md-accent hime" :href="resolveApkUrl(app.package_name, apk.version_code)" :md-ripple="false">
                             <md-icon>file_download</md-icon>
                             <span class="md-xsmall-hide">Download</span>
@@ -75,7 +75,6 @@
     }
 
     .truncated {
-        min-width: 0; /* or some value */
         span {
             white-space: nowrap;
             overflow: hidden;
