3ba1f7bb97228860d4d1e843bf114848ac104c48
Amatsuka Mao <maticalssh@gmail.com>
2018-05-13 00:43:36 +0800

    Update colors

--- resources/views/ng-layouts/master.blade.php
+++ resources/views/ng-layouts/master.blade.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="theme-color" content="#ff9800">
+    <meta name="theme-color" content="#d86b0d">
 
     <meta name="description" content="@yield('meta-description', 'ksmz is mine.')">
 
