5fa9a8590ca53066ea89f5d8f9e5eda7640d5d80
Amatsuka Mao <maticalssh@gmail.com>
2018-05-13 00:43:21 +0800

    Bump lock

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -2,7 +2,10 @@ import Vue from 'vue';
 import VueMaterial from 'vue-material';
 
 Vue.use(VueMaterial);
+
 Vue.component('himekawa', require('./components/Himekawa'));
+Vue.component('app-item', require('./components/AvailableApp'));
+Vue.component('apk', require('./components/Apk'));
 
 new Vue({
     el: '#app',
