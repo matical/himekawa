8ecc78c89f08b2c2562c61e5902d87e8ce727c39
Amatsuka Mao <maticalssh@gmail.com>
2018-05-13 02:01:37 +0800

    Apply fixes from StyleCI (#20)

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -22,7 +22,7 @@ class AppServiceProvider extends ServiceProvider
         if (app()->environment('production')) {
             URL::forceScheme('https');
         }
-        
+
 //        \DB::listen(function($query)
 //        {
 //            info("{$query->sql}");
