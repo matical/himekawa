954ebe01a49558d9a086c894523bdb410d7113f0
matical <maticalssh@gmail.com>
2018-07-06 18:50:45 +0800

    This is no longer required

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -16,8 +16,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        Schema::defaultStringLength(191);
-
         $this->registerBladeDirectives();
         if (app()->environment('production')) {
             URL::forceScheme('https');
