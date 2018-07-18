ace3c862ed74b1f94f1b5677c7df2d53d9bf4b05
matical <maticalssh@gmail.com>
2018-07-18 19:14:08 +0800

    Update faq resource

--- resources/views/frontend/faq.blade.php
+++ resources/views/frontend/faq.blade.php
@@ -7,7 +7,7 @@
         <p>The scraper runs every 15 minutes, from {{ config('himekawa.scheduler.start_time') }} to {{ config('himekawa.scheduler.end_time') }} JST.</p>
 
         <h2 class="md-title">Hashes</h2>
-        <p>Every hash (SHA1) displayed on each file is retrieved from GPlay (included in the payload metadata). The scraper will automatically verifies each downloaded file; if there is a hash mismatch, the file is automatically discarded. (that means if it doesn't verify something is borked -- let me know thanks) </p>
+        <p>Hashes are fetched directly from Google Play's servers. The scraper will automatically discard files that don't verify so hopefully </p>
 
         <h2 class="md-title">APK Lifetime</h2>
         <p>The system will keep up to 5 APKs per game.</p>
