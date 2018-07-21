3a62f9ecea34967807e3a521f74015fbd16d9d7d
matical <maticalssh@gmail.com>
2018-07-21 22:23:28 +0800

    Only import required components

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -1,11 +1,29 @@
 import Vue from 'vue';
-import VueMaterial from 'vue-material';
+import {
+    MdApp,
+    MdIcon,
+    MdList,
+    MdButton,
+    MdAvatar,
+    MdDrawer,
+    MdContent,
+    MdToolbar,
+    MdElevation,
+} from 'vue-material/dist/components';
 
 import yuki from './components/ShortLinks/Yuki';
 import himekawa from './components/Index/Himekawa';
 import mixins from './mixins';
 
-Vue.use(VueMaterial);
+Vue.use(MdApp);
+Vue.use(MdIcon);
+Vue.use(MdList);
+Vue.use(MdDrawer);
+Vue.use(MdButton);
+Vue.use(MdAvatar);
+Vue.use(MdToolbar);
+Vue.use(MdContent);
+Vue.use(MdElevation);
 
 Vue.mixin({methods: mixins});
 
