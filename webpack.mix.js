e237487e9b6a9c5b79404b9d9c040cda0de1f494
matical <maticalssh@gmail.com>
2019-02-03 22:11:37 +0800

    Fix css bugs

--- webpack.mix.js
+++ webpack.mix.js
@@ -3,11 +3,12 @@ let mix = require('laravel-mix');
 mix.copy('resources/images/*.png', 'public/images')
    .js('resources/js/app.js', 'public/js')
    .sass('resources/sass/app.scss', 'public/css')
-   .sourceMaps(false)
    .options({
        postCss: [
-           require('postcss-custom-properties')
-       ]
+           require('postcss-custom-properties')({
+               preserve: false,
+           }),
+       ],
    });
 
 
@@ -17,15 +18,21 @@ if (mix.inProduction()) {
     mix.version();
 }
 
+if (! mix.inProduction()) {
+    mix.webpackConfig({
+        devtool: 'inline-source-map',
+    }).sourceMaps();
+}
+
 mix.webpackConfig(webpack => {
     return {
         plugins: [
             new webpack.IgnorePlugin({
                 resourceRegExp: /^\.\/locale$/,
-                contextRegExp: /moment$/
-            })
-        ]
-    }
+                contextRegExp: /moment$/,
+            }),
+        ],
+    };
 });
 
 mix.browserSync({
@@ -33,12 +40,12 @@ mix.browserSync({
         target: 'localhost:8000',
         reqHeaders() {
             return {
-                host: 'localhost:3000'
+                host: 'localhost:3000',
             };
-        }
+        },
     },
     notify: false,
     open: false,
     online: false,
-    ui: false
+    ui: false,
 });
