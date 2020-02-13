b54b1cc32cb1f948eee184ba3363f966c82e791d
matical <maticalssh@gmail.com>
2020-02-13 13:11:08 +0800

    Spacing

--- app/Providers/HimeServiceProvider.php
+++ app/Providers/HimeServiceProvider.php
@@ -28,9 +28,15 @@ class HimeServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        $this->app->bind(Apk::class, fn () => new Apk(config('googleplay')));
+        $this->app->bind(
+            Apk::class,
+            fn () => new Apk(config('googleplay'))
+        );
 
-        $this->app->bind(LastRun::class, fn () => new LastRun(config('himekawa'), $this->app->make('cache.store')));
+        $this->app->bind(
+            LastRun::class,
+            fn () => new LastRun(config('himekawa'), $this->app->make('cache.store'))
+        );
 
         $this->app->alias(Apk::class, 'apk');
         $this->app->alias(LastRun::class, 'lastRun');
