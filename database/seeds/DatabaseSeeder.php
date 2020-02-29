e00c6d1e1cd92c06a6943a0fe915ffd6da1c9335
matical <maticalssh@gmail.com>
2020-02-29 23:48:31 +0800

    No longer needed

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,13 +12,6 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        User::create([
-            'name'           => 'sck',
-            'email'          => 'yuki@yuki.yuki',
-            'password'       => '$2y$10$Ia5E/Pj32peHAs0SbNKN9e0fGhFRm5LDmv6zh9FGSiVBelKfTGAqu', // changeme
-            'telegram_token' => env('TELEGRAM_BOT_ROUTE', null),
-        ]);
-
         $this->call(WatchedAppSeeder::class);
     }
 }
