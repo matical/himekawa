b19cbad9948c50b5e6b36ecc8d90db31263b2d0c
matical <matic@amatsuka.me>
2020-04-10 15:11:10 +0800

    Shortlinks component refactoring

---
+++ resources/js/components/ShortLinks/SingleApp.vue
@@ -0,0 +1,31 @@
+<template>
+    <md-list-item class="md-layout-item md-xlarge-size-20 md-large-size-33 md-small-size-50 md-xsmall-size-100">
+        <md-avatar><img :src="watchedApp.image"/></md-avatar>
+        <div class="md-list-item-text">
+            <p>{{ watchedApp.name }}</p>
+            <p
+                >v{{ watchedApp.available_apps[0].version_name }}
+                <span :class="{ recent: isRecent(watchedApp.available_apps[0].created_at) }">
+                    ({{ diffDate(watchedApp.updated_at) }})
+                </span>
+            </p>
+            <p
+                ><a :href="linkToShortRedirector(watchedApp.slug)">
+                    {{ linkToShortRedirector(watchedApp.slug) }}
+                </a></p
+            >
+        </div>
+    </md-list-item>
+</template>
+
+<script>
+export default {
+    props: ['watchedApp'],
+
+    methods: {
+        linkToShortRedirector(slug) {
+            return `${location.origin}/${slug}`;
+        },
+    },
+};
+</script>
--- resources/js/components/ShortLinks/Yuki.vue
+++ resources/js/components/ShortLinks/Yuki.vue
@@ -1,47 +1,23 @@
 <template>
-    <div class="s-container">
-        <h2 class="md-headline">Short Links</h2>
-        <p>Links here will always redirect you to the latest available APK version.</p>
-        <md-list class="md-double-line">
-            <div class="md-layout md-layout-nowrap md-gutter">
-                <md-list-item
-                    v-for="watched in availableApps"
-                    :key="watched.id"
-                    class="md-layout-item md-xlarge-size-20 md-large-size-33 md-small-size-50 md-xsmall-size-100"
-                >
-                    <md-avatar><img :src="watched.image"/></md-avatar>
-                    <div class="md-list-item-text">
-                        <span>{{ watched.name }}</span>
-                        <span
-                            >v{{ watched.available_apps[0].version_name }}
-                            <span :class="{ recent: isRecent(watched.available_apps[0].created_at) }">
-                                ({{ diffDate(watched.updated_at) }})
-                            </span>
-                        </span>
-                        <span
-                            ><a :href="linkToShortRedirector(watched.slug)">
-                                {{ linkToShortRedirector(watched.slug) }}
-                            </a></span
-                        >
-                    </div>
-                </md-list-item>
-            </div>
-        </md-list>
+    <div class="md-layout md-layout-nowrap md-gutter">
+        <single-app v-for="watched in apps" :key="watched.id" :watched-app="watched"></single-app>
     </div>
 </template>
 
 <script>
+import SingleApp from './SingleApp';
+
 export default {
-    props: ['availableApps'],
+    props: ['watchedApps'],
+    components: { SingleApp },
     data() {
         return {
+            apps: {},
             hovered: false,
         };
     },
-    methods: {
-        linkToShortRedirector(slug) {
-            return `${location.origin}/${slug}`;
-        },
+    created() {
+        this.apps = this.watchedApps.filter(app => app.available_apps.length > 0);
     },
 };
 </script>
