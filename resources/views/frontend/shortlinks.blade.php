58f851497d91028b9b2a71f50d8948c1c1e790e8
matical <matic@amatsuka.me>
2020-04-21 20:27:18 +0800

    Pull out from component

--- resources/views/frontend/shortlinks.blade.php
+++ resources/views/frontend/shortlinks.blade.php
@@ -2,5 +2,11 @@
 @section('title', 'Short Links')
 
 @section('content')
-    <yuki :available-apps='@json($apps)'></yuki>
+    <div class="s-container">
+        <h2 class="md-headline">Short Links</h2>
+        <p>Links here will always redirect you to the latest available APK version.</p>
+        <md-list class="md-double-line">
+            <yuki :watched-apps='@json($apps)'></yuki>
+        </md-list>
+    </div>
 @endsection
