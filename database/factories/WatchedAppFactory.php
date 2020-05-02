21c91909e1a62d46ee45206ef2db5bff423275c8
matical <matic@amatsuka.me>
2020-05-02 09:41:05 +0800

    WatchedApp factory

--- database/factories/WatchedAppFactory.php
+++ database/factories/WatchedAppFactory.php
@@ -3,5 +3,8 @@
 use Faker\Generator as Faker;
 
 $factory->define(himekawa\WatchedApp::class, fn (Faker $faker) => [
-    //
+    'name'           => $faker->word,
+    'slug'           => $faker->slug,
+    'original_title' => $faker->words(3, true),
+    'package_name'   => $faker->word,
 ]);
