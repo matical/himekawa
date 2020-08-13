5f6b320b9f28144bfe4e7c7c7ca1057831349740
matical <matic@amatsuka.me>
2020-08-13 12:48:56 +0800

    Change default delay

--- config/googleplay.php
+++ config/googleplay.php
@@ -4,5 +4,5 @@ return [
     'apk_base_path'      => env('APK_PATH', storage_path('apks')),
     'metainfo_cache_ttl' => env('METAINFO_TTL', 15 * 60),
     'download_timeout'   => env('SCRAPER_TIMEOUT', 300),
-    'delay'              => env('SCRAPER_DELAY', 20),
+    'delay'              => env('SCRAPER_DELAY', 5),
 ];
